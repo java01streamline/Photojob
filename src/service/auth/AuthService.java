0ccd4ea8074f73d634b4a0fc8f842815e34a373c
Абитуриент <@Streamline-O03>
2019-01-20 18:38:31 +0200

    registration complete (Andrey)

--- src/service/auth/AuthService.java
+++ src/service/auth/AuthService.java
@@ -1,32 +1,117 @@
-
 package service.auth;
 
 import entities.user.User;
 import java.awt.Component;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class AuthService {
-    
-    public void saveUser(JPanel regPanel) throws IllegalArgumentException, IllegalAccessException{
-        Component []components = regPanel.getComponents();
+
+    public void saveUser(JPanel regPanel) throws IllegalArgumentException, IllegalAccessException {
+        Component[] components = regPanel.getComponents();
         User u = new User();
-        Field []fields = u.getClass().getDeclaredFields();
-        for(int i = 0; i < components.length; i++){
-            if(!components[i].getClass().equals(JTextField.class)) continue;
-            
-            for(int j = 0; j < fields.length;j++){
-                if(!components[i].getName().equals(fields[j].getName())) continue;
+        Field[] fields = u.getClass().getDeclaredFields();
+        for (int i = 0; i < components.length; i++) {
+            if (!components[i].getClass().equals(JTextField.class)) {
+                continue;
+            }
+
+            for (int j = 0; j < fields.length; j++) {
+                if (!components[i].getName().equals(fields[j].getName())) {
+                    continue;
+                }
                 fields[j].setAccessible(true);
-                fields[j].set(u, ((JTextField)components[i]).getText());
+                fields[j].set(u, ((JTextField) components[i]).getText());
                 fields[j].setAccessible(false);
                 break;
             }
         }
-        u.setId(UUID.randomUUID().toString());
         
-        System.out.println(u.toString());
+        if(isUserExists(u)) return;
+        
+        u.setId(UUID.randomUUID().toString());
+
+        //System.out.println(u.toString());
+        File db = new File("users.txt");
+        if (!db.exists()) {
+            try {
+                db.createNewFile();
+            } catch (IOException ex) {
+                Logger.getLogger(AuthService.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        BufferedWriter bw = null;
+        try {
+             bw = new BufferedWriter(new FileWriter(db, true));
+        } catch (IOException ex) {
+            Logger.getLogger(AuthService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.
+                append(u.getId())
+                .append("\t")
+                .append(u.getName())
+                .append("\t")
+                .append(u.getSurname())
+                .append("\t")
+                .append(u.getLogin())
+                .append("\t")
+                .append(u.getEmail())
+                .append("\t")
+                .append(u.getPassword());
+        
+        try {
+            bw.write(sb.toString());
+            bw.newLine();
+        } catch (IOException ex) {
+            Logger.getLogger(AuthService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(AuthService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    private boolean isUserExists(User u){
+        
+        File db = new File("users.txt");
+        
+        BufferedReader br = null;
+        
+        try {
+            br = new BufferedReader(new FileReader(db));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(AuthService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        String line;
+        try {
+            while((line = br.readLine()) != null){
+                if(line.contains(u.getLogin())) return true;
+                if(line.contains(u.getEmail())) return true;
+            }   
+        } catch (IOException ex) {
+            Logger.getLogger(AuthService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            br.close();
+        } catch (IOException ex) {
+            Logger.getLogger(AuthService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 }
