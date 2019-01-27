91c14b5d6f1638a5f3deb8b053f61f50a46660c0
Admin <Admin@Streamline-O08>
2019-01-27 16:18:17 +0200

    Contrast(Roma)

--- src/frames/Main.java
+++ src/frames/Main.java
@@ -25,7 +25,7 @@ public class Main extends javax.swing.JFrame {
 
     private BufferedImage im;
     private Service service;
-    
+
     public Main() {
         this.service = new Service();
         this.im = null;
@@ -39,10 +39,17 @@ public class Main extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jSlider1 = new javax.swing.JSlider();
+        jSlider2 = new javax.swing.JSlider();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         photoField = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
         jButton1.setText("Load");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -57,6 +64,30 @@ public class Main extends javax.swing.JFrame {
             }
         });
 
+        jSlider2.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                jSlider2StateChanged(evt);
+            }
+        });
+
+        jLabel1.setText("Vividness");
+
+        jLabel2.setText("Contrast");
+
+        jButton2.setText("Mirror height");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Mirror widht ");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -65,15 +96,37 @@ public class Main extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jButton1)
-                    .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(372, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jSlider2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton3)
+                            .addComponent(jButton2))))
+                .addContainerGap(243, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 310, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jSlider1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jButton2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jSlider2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 281, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addContainerGap())
         );
@@ -131,6 +184,7 @@ public class Main extends javax.swing.JFrame {
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
+       
         display();
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -141,17 +195,103 @@ public class Main extends javax.swing.JFrame {
         if (photoField.getIcon() == null) {
             return;
         }
+        
         for (int i = 0; i < im.getWidth(); i++) {
             for (int j = 0; j < im.getHeight(); j++) {
                 Color current = new Color(im.getRGB(i, j));
-                Color newColor = new Color(service.RGBUp(current.getRed()), 
-                        service.RGBUp(current.getGreen()), 
+                Color newColor = new Color(service.RGBUp(current.getRed()),
+                        service.RGBUp(current.getGreen()),
                         service.RGBUp(current.getBlue()));
                 im.setRGB(i, j, newColor.getRGB());
             }
         }
         display();
     }//GEN-LAST:event_jSlider1StateChanged
+
+    private void jSlider2StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSlider2StateChanged
+        if (im == null) {
+            return;
+        }
+        if (photoField.getIcon() == null) {
+            return;
+        }
+        int s= jSlider1.getWidth();
+        for (int i = 0; i < im.getWidth(); i++) {
+            for (int j = 0; j < im.getHeight(); j++) {
+                Color current = new Color(im.getRGB(i, j));
+                if ( current.getGreen() <= 125 || current.getRed() >= 125) {
+                    Color newColor = new Color(service.RGBDown(current.getRed()),
+                            service.RGBDown(current.getGreen()),
+                            service.RGBDown(current.getBlue()));
+                    im.setRGB(i, j, newColor.getRGB());
+                } else {
+                    Color newColor = new Color(service.RGBUp(current.getRed()),
+                            service.RGBUp(current.getGreen()),
+                            service.RGBUp(current.getBlue()));
+                    im.setRGB(i, j, newColor.getRGB());
+                }
+            }
+        }
+        
+       
+        display();
+    }//GEN-LAST:event_jSlider2StateChanged
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        if (im == null) {
+            return;
+        }
+        if (photoField.getIcon() == null) {
+            return;
+        }
+      
+        for (int i = 0; i < im.getWidth(); i++) {
+            for (int j = 0; j < im.getHeight()/2; j++) {
+                Color current = new Color(im.getRGB(i, j));
+                
+                Color current1 = new Color(im.getRGB(i, im.getHeight()-1- j));
+                int red1=current1.getRed();
+                int blue1=current1.getBlue();
+                int green1=current1.getGreen();    
+                Color newColor1 = new Color(current.getRed(),current.getGreen(),current.getBlue());
+                    im.setRGB(i, im.getHeight()-1- j, newColor1.getRGB());
+                Color newColor = new Color(red1,green1, blue1);
+                    im.setRGB(i,j, newColor.getRGB());    
+            
+            }
+        }
+        
+       
+        display();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        if (im == null) {
+            return;
+        }
+        if (photoField.getIcon() == null) {
+            return;
+        }
+      
+        for (int i = 0; i < im.getWidth()/2; i++) {
+            for (int j = 0; j < im.getHeight(); j++) {
+                Color current = new Color(im.getRGB(i, j));
+                
+                Color current1 = new Color(im.getRGB(im.getWidth()-1- i, j));
+                int red1=current1.getRed();
+                int blue1=current1.getBlue();
+                int green1=current1.getGreen();    
+                Color newColor1 = new Color(current.getRed(),current.getGreen(),current.getBlue());
+                    im.setRGB( im.getWidth()-1-i, j, newColor1.getRGB());
+                Color newColor = new Color(red1,green1, blue1);
+                    im.setRGB(i,j, newColor.getRGB());    
+            
+            }
+        }
+        
+       
+        display();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     private void display() {
         ImageIcon icon = new ImageIcon(im.getScaledInstance(photoField.getWidth(),
@@ -161,9 +301,14 @@ public class Main extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JSlider jSlider1;
+    private javax.swing.JSlider jSlider2;
     private javax.swing.JLabel photoField;
     // End of variables declaration//GEN-END:variables
 }
