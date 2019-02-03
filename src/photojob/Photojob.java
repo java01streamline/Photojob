eb29484514888f6060b466fcd9b36be6d21ac8a7
java.01.streamline@gmail.com <streamline01java>
2019-02-03 23:02:37 +0200

    Bugs fixed (Valera)

--- src/photojob/Photojob.java
+++ src/photojob/Photojob.java
@@ -2,14 +2,11 @@
 package photojob;
 
 import frames.Authorization;
-import frames.Main;
-import frames.open_error;
-
 
 public class Photojob {
 
     public static void main(String[] args) {
-        new open_error().setVisible(true);
+        
         new Authorization().setVisible(true);
         
     }
--- src/service/database/DatabaseService.java
+++ src/service/database/DatabaseService.java
@@ -2,32 +2,23 @@ package service.database;
 
 import entities.user.User;
 import java.io.BufferedReader;
-<<<<<<< HEAD
 import java.io.BufferedWriter;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-
-=======
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
->>>>>>> origin/master
-import service.auth.AuthService;
 
 public class DatabaseService {
 
     private String db = "users.txt";
-<<<<<<< HEAD
-   public User toUser(String dataAll){
+
+    public User toUser(String dataAll) {
         User ret = new User();
-        String []data = dataAll.split("\t");
+        String[] data = dataAll.split("\t");
         ret.setId(data[0]); //id
         ret.setName(data[1]);   //name
         ret.setSurname(data[2]);    //surname
@@ -37,58 +28,50 @@ public class DatabaseService {
         return ret;
     }
 
-    public User findUserById(String id) throws IOException {
+    public User findUserById(String id){
         File f = new File(db);
         if (!f.exists()) {
             return null;
         }
-        BufferedReader r = new BufferedReader(new FileReader(f));
-        String line;
-        while ((line = r.readLine()) != null) {
-            if (toUser(line).getId().equals(id)) {
-                r.close();
-                return toUser(line);
-
-            }
+        BufferedReader r = null;
+        try {
+            r = new BufferedReader(new FileReader(f));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(DatabaseService.class.getName()).log(Level.SEVERE, null, ex);
         }
-        r.close();
+        String line;
+        try {
+            while ((line = r.readLine()) != null) {
+                if (toUser(line).getId().equals(id)) {
+                    r.close();
+                    return toUser(line);
+                    
+                }
+            }
+            r.close();
+        } catch (IOException ex) {
+            Logger.getLogger(DatabaseService.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
     }
 
-    public User findUserByEmail(String email) {
-=======
-   //private AuthService authService = new AuthService();
-   public User toUser(String dataAll){
-        User ret = new User();
-        String []data = dataAll.split("\t");
-        ret.setId(data[0]); //id
-        ret.setName(data[1]);   //name
-        ret.setSurname(data[2]);    //surname
-        ret.setLogin(data[3]);  //login
-        ret.setEmail(data[4]);  //email
-        ret.setPassword(data[5]);   //password
-        return ret;
-    }
-   
- 
-    public User findUserById(String id){
->>>>>>> origin/master
-        throw new UnsupportedOperationException();
-    }
-<<<<<<< HEAD
+    public void updateUserProfile(User user) {
+        User au = findUserById(user.getId());
 
-    public void updateUserProfile(User user) throws FileNotFoundException, IOException {
-        User au=findUserById(user.getId());
-       
         saveUser(user);
     }
 
     public boolean isUserExists(User user) {
-        throw new UnsupportedOperationException();
-=======
-    
-    public User findUserByEmail(String email){
-       
+        ArrayList<User> users = getAllUsers();
+        for(int i = 0; i < users.size(); i++){
+            if(users.get(i).getEmail().equals(user.getEmail()) || users.get(i).getLogin().equals(user.getLogin())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public User findUserByEmail(String email) {
         File f = new File(db);
         BufferedReader br = null;
         try {
@@ -98,47 +81,68 @@ public class DatabaseService {
         }
         String line;
         try {
-            while((line = br.readLine()) != null){
-                if(toUser(line).getEmail().equals(email)) return toUser(line);
+            while ((line = br.readLine()) != null) {
+                if (toUser(line).getEmail().equals(email)) {
+                    return toUser(line);
+                }
             }
         } catch (IOException ex) {
             Logger.getLogger(DatabaseService.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
->>>>>>> origin/master
     }
 
-    public User[] allUsers() {
-        throw new UnsupportedOperationException();
+    public ArrayList<User> getAllUsers() {
+        ArrayList<User> ret = new ArrayList<>();
+        File f = new File(db);
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(f));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(DatabaseService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                ret.add(toUser(line));
+            }
+            br.close();
+        } catch (IOException ex) {
+            Logger.getLogger(DatabaseService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ret;
     }
 
-    public void saveUser(User user) throws IOException {
+    public void saveUser(User user) {
         File f = new File(db);
         if (!f.exists()) {
             return;
-        } 
-        
-        
-       
+        }
         String line;
-        BufferedWriter w = new BufferedWriter(new FileWriter(f));
-       
-                w.write(new StringBuilder()
-                .append(user.getId())
-                .append("\t")
-                .append(user.getName())
-                .append("\t")
-                .append(user.getSurname())
-                .append("\t")
-                .append(user.getLogin())
-                .append("\t")
-                .append(user.getEmail())
-                .append("\t")
-                .append(user.getPassword())
-                .append("\t").toString());
-
-            
-        w.close();
+        BufferedWriter w = null;
+        try {
+            w = new BufferedWriter(new FileWriter(f));
+        } catch (IOException ex) {
+            Logger.getLogger(DatabaseService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            w.write(new StringBuilder()
+                    .append(user.getId())
+                    .append("\t")
+                    .append(user.getName())
+                    .append("\t")
+                    .append(user.getSurname())
+                    .append("\t")
+                    .append(user.getLogin())
+                    .append("\t")
+                    .append(user.getEmail())
+                    .append("\t")
+                    .append(user.getPassword())
+                    .append("\t").toString());
+            w.close();
+        } catch (IOException ex) {
+            Logger.getLogger(DatabaseService.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 }
